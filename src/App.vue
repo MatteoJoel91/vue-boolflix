b7b2eb8a063c62c2d2f3d708d71c096d3822006c
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-02-25 16:43:45 +0100

    push

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,47 @@
 </template>
 
 <script>
+
+const axios = require('axios');
+
 import MyHeader from './components/MyHeader.vue'
 import MyMain from './components/MyMain.vue'
 import MyFooter from './components/MyFooter.vue'
 
 export default {
   name: 'App',
+
+  data(){
+    return{
+        listaFilm: [],
+        cercaFilm: '',
+    }
+  },
+
   components: {
     MyHeader,
     MyMain,
-    MyFooter
+    MyFooter,
+
+  },
+  
+  methods:{
+    getFilm(){
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((response) => {
+            // handle success
+            this.listaFilm = response.data.response;
+            console.log(response);
+            console.log(this.listaFilm);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+    }
   }
 }
 </script>
