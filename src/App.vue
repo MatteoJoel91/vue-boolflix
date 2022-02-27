bc2972a008b064103f552c81c7a9e2bf7ee327b5
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-02-27 13:46:13 +0100

    milestone 2 e 3 90% completate

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <MyHeader @ricerca='effettuaRicerca' />
-    <MyMain :listaFilmProps="listaFilm"/>
+    <MyMain 
+    :listaFilmProps="listaFilm"
+    :listaSerieProps="listaSerie"
+    :votiInteriProps="votiInteri"
+
+    />
       <!-- <input type="text" v-model="cercaFilm"> 
       <button @click="effettuaRicerca">ricerca</button> -->
 
@@ -11,7 +16,6 @@
           <li>Lingua originale: {{film.original_language}}</li>
           <li>Voto: {{film.vote_average}}</li><br>
       </ul> -->
-     
     <MyFooter />
     
   </div>
@@ -31,7 +35,12 @@ export default {
   data(){
     return{
         cercaFilm:'',      
-        listaFilm: [],       
+        listaFilm: [],
+        listaSerie:[],
+        voti:[],
+        votoDiviso: '',
+        votiInteri:[],
+        
     }
   },
 
@@ -48,16 +57,43 @@ export default {
         .then((response) => {
             // handle success
             this.listaFilm = response.data.results;
-            console.log(response);
             console.log(this.listaFilm);
+            for (let i = 0; i < this.listaFilm.length; i++) {
+              this.voti.push(this.listaFilm[i].vote_average)
+            }
+            for (let j = 0; j < this.voti.length; j++) {
+                  this.votoDiviso = parseInt(this.voti[j] / 2);
+                  this.votiInteri.push(this.votoDiviso);
+            }
+            console.log(this.votiInteri);
+      })
+    },
+
+    getSerie(text){
+      axios.get(`https://api.themoviedb.org/3/search/tv?api_key=9cebc562ed1232cc2dd23bfe6f97bc80&query=${text}&language=it-IT`)
+        .then((response) => {
+            // handle success
+            this.listaSerie = response.data.results;
+            console.log(this.listaSerie);
+            // for (let i = 0; i < this.listaSerie.length; i++) {
+            //   this.voti.push(this.listaSerie[i].vote_average)
+            // }
+            // for (let j = 0; j < this.voti.length; j++) {
+            //       this.votoDiviso = parseInt(this.voti[j] / 2);
+            //       this.votiInteri.push(this.votoDiviso);
+            // }
+            // console.log(this.votiInteri);
       })
     },
 
     effettuaRicerca(valore) {
       this.cercaFilm = valore;
       this.getFilm(this.cercaFilm);
+      this.getSerie(this.cercaFilm);
       this.cercaFilm = "";
     },
+
+
   },
   // created(){
   //   this.getFilm();
--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,18 +1,36 @@
 <template>
-    <div>
-        <ul v-for="(film, index) in listaFilmProps" :key="index">
-          <li>Titolo: {{film.title}}</li>
-          <li>Titolo originale: {{film.original_title}}</li>
-          <li>Lingua originale: <lang-flag :iso="film.original_language" /></li>
-          <li>Voto: {{film.vote_average}}</li><br>
-      </ul>
+    <div >
+        <h1>Lista Film</h1>
+        <section v-for="(film, index) in listaFilmProps" :key="index">
+            <ul>
+                <img :src="`https://image.tmdb.org/t/p/w45/${film.poster_path}`" alt="">
+                <li>Titolo: {{film.title}}</li>
+                <li>Titolo originale: {{film.original_title}}</li>
+                <li>Lingua originale: <lang-flag :iso="film.original_language" /></li><br>
+                <!-- <li>Voto: <i class="fa-solid fa-star"  v-for="(number, index) in votiInteriProps" :key="index"></i></li><br> -->
+            </ul>
+        </section>
+        <br>
+        <h1>Lista Serie Tv</h1>
+        <section v-for="(serie) in listaSerieProps" :key="serie.id">
+            <ul>
+                <img :src="`https://image.tmdb.org/t/p/w45/${serie.poster_path}`" alt="">
+                <li>Titolo: {{serie.name}}</li>
+                <li>Titolo originale: {{serie.original_name}}</li>
+                <li>Lingua originale: <lang-flag :iso="serie.original_language" /></li>
+                <li>Voto: <i class="fa-solid fa-star"></i></li><br>
+            </ul>
+        </section>
     </div>
 </template>
 
 <script>
 export default {
     name: 'MyMain',
-    props: ['listaFilmProps'],
+    props: ['listaFilmProps',
+            'listaSerieProps',
+            'votiInteriProps',
+            ] ,
     
     data(){
         return{
